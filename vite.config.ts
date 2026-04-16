1253fe8534d59d37d2b1e6ee118090e8116bc306
Adishwar Rishi <adiswa123@gmail.com>
2026-04-16 13:12:32 +1000

    chore: Upgrade all packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,17 @@ import { devtools } from '@tanstack/devtools-vite'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 import { sitemapPlugin } from './src/lib/vite-sitemap-plugin'
 
 const config = defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     devtools(),
     nitro(),
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
